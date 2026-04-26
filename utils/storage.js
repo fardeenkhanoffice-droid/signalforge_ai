a4b7a7b7935a6d7a2c35e69b39dba4bc26f54d22
fardeenkhanoffice-droid <fardeenkhanoffice@gmail.com>
2026-04-26 15:28:32 +0530

    Add functions to save and retrieve trades from localStorage

---
+++ utils/storage.js
@@ -0,0 +1,9 @@
+export function saveTrade(trade) {
+  const history = JSON.parse(localStorage.getItem("trades")) || [];
+  history.push(trade);
+  localStorage.setItem("trades", JSON.stringify(history));
+}
+
+export function getTrades() {
+  return JSON.parse(localStorage.getItem("trades")) || [];
+}
