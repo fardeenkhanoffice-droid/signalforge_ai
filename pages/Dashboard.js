8c3d5b617bb57fc72da75b80350bdaf972b06d49
fardeenkhanoffice-droid <fardeenkhanoffice@gmail.com>
2026-04-26 15:37:27 +0530

    Update Dashboard.js

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -2,11 +2,13 @@ import Header from "../components/Header.js";
 import RiskCard from "../components/RiskCard.js";
 import SignalCard from "../components/SignalCard.js";
 import AnalyzeButton from "../components/AnalyzeButton.js";
+import AnalyticsCard from "../components/AnalyticsCard.js";
 
 export default function Dashboard() {
   return `
     ${Header()}
     ${RiskCard()}
+    ${AnalyticsCard()}
     ${SignalCard()}
     ${AnalyzeButton()}
   `;
