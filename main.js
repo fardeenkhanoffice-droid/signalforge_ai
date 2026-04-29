5a1ed194b32ea5e036be0a9ae46361320eeb7e59
fardeenkhanoffice-droid <fardeenkhanoffice@gmail.com>
2026-04-29 15:04:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 import App from "./App.js";
 
 const root = document.getElementById("root");
-root.innerHTML = App();
+
+try {
+  root.innerHTML = App();
+} catch (e) {
+  root.innerHTML = `
+    <h1 style="color:red">App Crashed</h1>
+    <pre>${e.message}</pre>
+  `;
+  console.error(e);
+}
